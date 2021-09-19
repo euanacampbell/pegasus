0382e199e1817dd0be6d4cbc89b777e8f8f089cc
Euan Campbell <euanalisdaircampbell@gmail.com>
2021-09-19 21:21:34 +0100

    query sorting fixed

--- pegasus/modules/sql.py
+++ pegasus/modules/sql.py
@@ -65,7 +65,8 @@ class sql:
         """Takes a given command/param and runs it"""
 
         if command in self.commands:
-            queries = self.commands[command]['queries'].sort()
+            queries = self.commands[command]['queries']
+            queries.sort()
         elif command in self.queries:
             queries = [command]
 
