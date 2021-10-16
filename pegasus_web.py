7d2f703fae9a972a228d21879484428695d5e00d
Euan Campbell <euanalisdaircampbell@gmail.com>
2021-10-16 16:57:40 +0100

    flask fix

--- pegasus_web.py
+++ pegasus_web.py
@@ -14,4 +14,5 @@ app.register_blueprint(core_routes)
 app.register_blueprint(error_routes)
 
 
-app.run(debug=True, host='127.0.0.1', port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
