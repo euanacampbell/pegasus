794075f88ffc2da60faa2f92772a1e6804a2d336
Euan Campbell <euanalisdaircampbell@gmail.com>
2022-05-08 23:39:36 +0100

    snippets fix

--- pegasus_client/default_modules/generic/snippets.py
+++ pegasus_client/default_modules/generic/snippets.py
@@ -37,6 +37,6 @@ class ClassName:
 
 
 '''arg'''
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
 '''arg'''
